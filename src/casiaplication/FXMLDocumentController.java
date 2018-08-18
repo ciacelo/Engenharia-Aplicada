39f5cc95208b2731047528410748cf669b5fbdf9
ciacelo <marcelo.martins@dce.ufpb.br>
2018-08-18 14:45:35 -0300

    Atualizando GUI

--- src/casiaplication/FXMLDocumentController.java
+++ src/casiaplication/FXMLDocumentController.java
@@ -8,6 +8,7 @@ package casiaplication;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import com.sun.glass.events.WindowEvent;
 import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,12 +16,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 /**
  *
  * @author ciacelo
  */
 public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private AnchorPane anchorPane;
     
     @FXML
     private JFXTextField txtUserName;
@@ -33,6 +41,16 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private JFXButton btnClose;
+    
+    @FXML
+    private JFXButton btnMinimize;
+    
+    @FXML
+    public void handlerActionMinimize(ActionEvent mouseEvent){
+        Stage stage = (Stage)anchorPane.getScene().getWindow();
+        stage.setIconified(true);
+
+    }
     
     @FXML
     public void handlerMouseClicked(ActionEvent event){

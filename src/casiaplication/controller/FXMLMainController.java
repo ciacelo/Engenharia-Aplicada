ca00ef97ac61ad90f34421823252e6c644cf2047
ciacelo <marcelo.martins@dce.ufpb.br>
2018-09-18 13:59:13 -0300

    Atualizando Controller

---
+++ src/casiaplication/controller/FXMLMainController.java
@@ -0,0 +1,27 @@
+package casiaplication.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class FXMLMainController {
+
+    @FXML
+    private JFXButton btnEvents;
+
+    @FXML
+    private JFXButton btnAtividade;
+
+    @FXML
+    private JFXButton btnMain;
+
+    @FXML
+    private ImageView btnMin;
+
+    @FXML
+    public void handlerMouseMin(MouseEvent event) {
+
+    }
+
+}

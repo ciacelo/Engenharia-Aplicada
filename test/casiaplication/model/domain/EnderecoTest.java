6f654d750d9d3ad8bfb7b06c086d952466841821
Douglas Cardins <douglas.cardins@gmail.com>
2018-10-27 18:08:54 -0300

    EndeceroTest implementado

--- test/casiaplication/model/domain/EnderecoTest.java
+++ test/casiaplication/model/domain/EnderecoTest.java
@@ -6,34 +6,44 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EnderecoTest {
-
+	Endereco end;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		 end = new Endereco("Rua martins da vila", "616", "Rio Tinto", "Paraiba");
 	}
 
 	@Test
 	void testEndereco() {
-		fail("Not yet implemented");
+		assertEquals("Rua martins da vila", end.getLogradouro());
+		assertEquals("616", end.getNumero());
+		assertEquals("Rio Tinto", end.getCidade());
+		assertEquals("Paraiba", end.getEstado());
+		
 	}
 
 	@Test
 	void testGetLogradouro() {
-		fail("Not yet implemented");
+		end.setLogradouro("Rio do norte");
+		assertEquals("Rio do norte", end.getLogradouro());
 	}
 
 	@Test
 	void testGetNumero() {
-		fail("Not yet implemented");
+		end.setNumero("161");
+		assertEquals("161", end.getNumero());
 	}
 
 	@Test
 	void testGetCidade() {
-		fail("Not yet implemented");
+		end.setCidade("Jurucutu");
+		assertEquals("Jurucutu", end.getCidade());
 	}
 
 	@Test
 	void testGetEstado() {
-		fail("Not yet implemented");
+		end.setEstado("RN");
+		assertEquals("RN", end.getEstado());
 	}
 
 }
